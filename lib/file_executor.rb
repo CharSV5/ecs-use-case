b2296b9f4ecedc201d2b18cfce7e74c60d293f3f
CharSV5 <charlenebastians@hotmail.com>
2019-01-09 16:43:17 +0000

    refactored

--- lib/file_executor.rb
+++ lib/file_executor.rb
@@ -2,43 +2,43 @@
 
 require 'mysql2'
 
-class File_executor
+class FileExecutor
 
   def db_details
     details = ARGV
     if details.length < 4
-      puts "Too few arguments"
+      puts 'Too few arguments'
       exit
     end
-    #details[0] = directory with sql scripts
-    #details[1] = username for the db
-    #details[2] = db host
-    #details[3] = db name
-    #deatils[4] = db password
+    # details[0] = directory with sql scripts
+    # details[1] = username for the db
+    # details[2] = db host
+    # details[3] = db name
+    # deatils[4] = db password
     details
   end
 
   def open_db
-    Mysql2::Client.new(:host => db_details[2], :username => db_details[1], :password => db_details[4], :database => db_details[3])
+    Mysql2::Client.new(:host => db_details[2], :username => db_details[1],
+      :password => db_details[4], :database => db_details[3])
   end
 
   def create_table
     client = open_db
     client.query("CREATE TABLE IF NOT EXISTS \
     VersionTable(Id INT PRIMARY KEY AUTO_INCREMENT, Version VARCHAR(25))")
-    client.query("INSERT IGNORE INTO VersionTable(Version) VALUES('1.createtable.sql')")
-  rescue Mysql2::Error => e
-    puts e.errno
-    puts e.error
-  ensure
-    client.close if client
+    client.query("INSERT INTO VersionTable(Version) \
+     VALUES('1.createtable.sql')")
+     client.close if client
   end
 
   def latest_version
     client = open_db
-    latest = client.query("SELECT Version FROM VersionTable WHERE ID = (SELECT MAX(ID) FROM VersionTable)")
+    latest = client.query("SELECT Version FROM VersionTable \
+      WHERE ID = (SELECT MAX(ID) FROM VersionTable)")
     version = ''
     latest.each { |row| version = row['Version'] }
+    client.close if client
     version
   end
 
@@ -52,7 +52,7 @@ class File_executor
     new_version_array = []
     all_files.each do
       |version| num = version.scan(/\d/).join('')
-      num > version_num ? new_version_array.push(version) : ''
+      new_version_array.push(version) if num > version_num
     end
     new_version_array
     end
@@ -63,14 +63,16 @@ class File_executor
       |version| sql = File.open(version, 'rb') { |file| file.read }
       client.query(sql)
     end
+    client.close if client
   end
 
   def update
     client = open_db
     new_versions.each do
       |version|
-      client.query("INSERT INTO VersionTable(Version) VALUES(?)", version)
+      client.query("INSERT INTO VersionTable(Version) VALUES('#{version.to_str}')")
     end
+    client.close if client
   end
 
 end
@@ -78,10 +80,11 @@ end
 
 
 
-file_executor = File_executor.new
-file_executor.db_details
-file_executor.create_table
-file_executor.all_files
-file_executor.latest_version
-file_executor.new_versions
-file_executor.execute_sql
+fileExecutor = FileExecutor.new
+fileExecutor.db_details
+fileExecutor.create_table
+fileExecutor.all_files
+fileExecutor.latest_version
+fileExecutor.new_versions
+fileExecutor.execute_sql
+fileExecutor.update
