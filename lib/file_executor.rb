817cfb13b9ba8d720d0c4589cf48154e5368e525
CharSV5 <charlenebastians@hotmail.com>
2019-01-09 12:21:05 +0000

    connect to database and create table if not exists

--- lib/file_executor.rb
+++ lib/file_executor.rb
@@ -1,3 +1,22 @@
+#!/usr/bin/ruby
+
+require 'mysql2'
+
+begin
+
+client = Mysql2::Client.new(:host => "localhost", :username => "charlene", :password => 'ecsdigital', :database => 'mydb')
+    client.query("CREATE TABLE IF NOT EXISTS \
+        VersionTable(Id INT PRIMARY KEY AUTO_INCREMENT, Version VARCHAR(25))")
+    client.query("INSERT IGNORE INTO VersionTable(Version) VALUES('1.createtable.sql ')")
+
+rescue Mysql2::Error => e
+    puts e.errno
+    puts e.error
+
+ensure
+    client.close if client
+end
+
 
 class File_executor
   def all_files
